0d51dcc25af49d4280bb2e3f3a2e492a639109b2
MEL1902 <1902otamel@gmail.com>
2023-10-31 12:49:10 +0900

    update20231031jenkins-tst2

--- src/main/java/com/playdata/cpuboundapp/controller/AppController.java
+++ src/main/java/com/playdata/cpuboundapp/controller/AppController.java
@@ -19,7 +19,7 @@ public class AppController {
     }
     @RequestMapping("/hello")
     public String hello() {
-        return "hello ci/cd test";
+        return "hello ci/cd";
     }
     private String getMD5Digest(String input) throws NoSuchAlgorithmException {
         MessageDigest md = MessageDigest.getInstance("MD5");
