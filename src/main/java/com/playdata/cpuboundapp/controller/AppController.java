6d0b7ce1e1bed28b1e0a03a736393b2f142a4e03
MEL1902 <1902otamel@gmail.com>
2023-11-01 11:31:00 +0900

    20231101update

--- src/main/java/com/playdata/cpuboundapp/controller/AppController.java
+++ src/main/java/com/playdata/cpuboundapp/controller/AppController.java
@@ -19,7 +19,7 @@ public class AppController {
     }
     @RequestMapping("/hello")
     public String hello() {
-        return "hello ci/cd";
+        return "hello ci/cdfghjhgf";
     }
     private String getMD5Digest(String input) throws NoSuchAlgorithmException {
         MessageDigest md = MessageDigest.getInstance("MD5");
